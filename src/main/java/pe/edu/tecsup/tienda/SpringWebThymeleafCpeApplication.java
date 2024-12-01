ba22088a3ea684124812121f41b304376897ecda
jgomezz <jgomezz@gmail.com>
2024-12-01 08:31:06 -0500

    first commit

---
+++ src/main/java/pe/edu/tecsup/tienda/SpringWebThymeleafCpeApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebThymeleafCpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebThymeleafCpeApplication.class, args);
+	}
+
+}
