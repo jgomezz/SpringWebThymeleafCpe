e610fe491768ff6c49df743dfe2ad41769001f94
jgomezz <jgomezz@gmail.com>
2024-12-01 08:55:15 -0500

    update

--- src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
@@ -1,4 +1,17 @@
 package pe.edu.tecsup.tienda.webs;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+@RestController
 public class ProductoController {
+
+    @GetMapping("")
+    public String home (){
+        return "Hello world again..!";
+    }
+
 }
