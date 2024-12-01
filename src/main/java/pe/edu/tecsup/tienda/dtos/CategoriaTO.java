9f359d456d4e4a6cf1a503e9a507076270c1e2e7
jgomezz <jgomezz@gmail.com>
2024-12-01 08:47:51 -0500

    update

--- src/main/java/pe/edu/tecsup/tienda/dtos/CategoriaTO.java
+++ src/main/java/pe/edu/tecsup/tienda/dtos/CategoriaTO.java
@@ -1,8 +1,13 @@
 package pe.edu.tecsup.tienda.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 public class CategoriaTO {
 
     private Long id;
